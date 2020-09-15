17a2e74494b219df055f396ae086302f20493e76
Ignatio <ignatio.julian@ti.ukdw.ac.id>
2020-09-16 00:21:23 +0700

    [ENHANCEMENT] add stories & video scroll, banner, editor spots & footer

--- function.js
+++ function.js
@@ -5,4 +5,62 @@ function setTheme() {
  }
  
 
- 
+//  Function Slider Stories
+$(function slider() {
+    $("#panLeft:first-of-type").click(
+        function() {
+            var navwidth = $("#list");
+            navwidth.scrollLeft(navwidth.scrollLeft() - 200);
+        }
+    );
+    $("#panRight:nth-of-type(2)").click(
+        function() {
+            var navwidth = $("#list");
+            navwidth.scrollLeft(navwidth.scrollLeft() + 200);
+        }
+    );  
+  });
+
+
+  //  Function Slider Video
+$(function slider() {
+    $("#panLeftVideo:first-of-type").click(
+        function() {
+            var navwidth = $("#listVideo");
+            navwidth.scrollLeft(navwidth.scrollLeft() - 200);
+        }
+    );
+    $("#panRightVideo:nth-of-type(2)").click(
+        function() {
+            var navwidth = $("#listVideo");
+            navwidth.scrollLeft(navwidth.scrollLeft() + 200);
+        }
+    );  
+  });
+
+
+//   Function change color when chip checkbox are clicked
+function changeColorChip1() {
+    var element = document.getElementById('chip1');
+    element.classList.toggle("changeBackground");
+ }
+
+ function changeColorChip2() {
+    var element = document.getElementById('chip2');
+    element.classList.toggle("changeBackground");
+ }
+
+ function changeColorChip3() {
+    var element = document.getElementById('chip3');
+    element.classList.toggle("changeBackground");
+ }
+
+ function changeColorChip4() {
+    var element = document.getElementById('chip4');
+    element.classList.toggle("changeBackground");
+ }
+
+ function changeColorChip5() {
+    var element = document.getElementById('chip5');
+    element.classList.toggle("changeBackground");
+ }
